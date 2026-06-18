405feeb9f6239fe874f800a879aff4406e7120a9
Shafa <shafaarshiya@gmail.com>
2026-06-18 14:08:30 +0530

    new push

--- app.py
+++ app.py
@@ -140,4 +140,8 @@ def api_overdue():
     return jsonify(overdue)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
